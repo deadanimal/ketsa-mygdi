41f6e01bd7606b43b7d9f8bad72c4a8ed9935ae1
Dinie Afeeq <dinieafiq@pipeline.com.my>
2021-04-26 18:43:32 +0800

    set

---
+++ web/src/app/shared/services/data/metadata.ts
@@ -0,0 +1,60 @@
+export class Metadata {
+  metadata_name: any;
+  product_type: any;
+  abstract: any;
+
+  contact_person: any;
+  addressline1: any;
+  addressline2: any;
+  addressline3: any;
+  postcode: any;
+  city: any;
+  state: any;
+  country: any;
+  email: any;
+  fax: any;
+  website: any;
+
+  west_bl: any;
+  south_bl: any;
+  east_bl: any;
+  north_bl: any;
+
+  filename: any;
+  filetype: any;
+  url: any;
+  keyword: any;
+  adkeyword1: any;
+  adkeyword2: any;
+
+  distribution_format: any;
+  version: any;
+  medium: any;
+  distributor: any;
+  unit_distribution: any;
+  sizemb: any;
+  fees: any;
+  weblink: any;
+  instruction_order: any;
+
+  dataset_type: any;
+  hardsoftcopy: any;
+  imggsd: any;
+  language: any;
+
+  system_identifier: any;
+
+  access_cons: any;
+  use_cons: any;
+  classification_system: any;
+  DQscope: any;
+  date: any;
+
+  scope: any;
+  compliance_lvl: any;
+  datecompleteness: any;
+  result:any;
+  conformance_result:any;
+  
+  
+}
