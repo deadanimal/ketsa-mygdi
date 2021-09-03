2f911a2e1986f98f147efcdfc7a3a6501a755630
farhanrimfiel <farhan.rimfiel@pipeline-network.com>
2021-09-03 15:46:40 +0800

    add cron for unchecked metadata over 2 weeks

--- app/Console/Commands/UncheckedMetadataCron.php
+++ app/Console/Commands/UncheckedMetadataCron.php
@@ -9,6 +9,8 @@ use App\Role;
 use App\ModelHasRoles;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\MailNotify;
+use Storage;
+use DB;
 
 class UncheckedMetadataCron extends Command
 {
@@ -43,20 +45,31 @@ class UncheckedMetadataCron extends Command
      */
     public function handle()
     {
-        $lastTwoWeeks = date('Y-m-d', strtotime("-2 weeks"));
-        
-        $query = MetadataGeo::on('pgsql2')->where('disahkan','0')->whereNull('cronned_metadata_tak_diusik');
-        $result = $query->whereDate('createdate','<',$lastTwoWeeks)->get();
-        if($result > 0){
-            foreach($result as $r){
-                $r->cronned_metadata_tak_diusik = date('Y-m-d H:i:s',strtotime());
+        $lastTwoWeeks = date('Y-m-d H:i:s', strtotime("-2 weeks"));
+       
+        //find metadata tak diusik lebih dari 2 minggu
+        $result1 = MetadataGeo::on('pgsql2')->whereRaw('createdate = changedate')->whereDate('createdate','<',$lastTwoWeeks)->whereNull('cronned_metadata_tak_diusik')->get();
+        if(count($result1) > 0){
+            foreach($result1 as $r){
+                $r->cronned_metadata_tak_diusik = date('Y-m-d H:i:s',time());
                 $r->update();
             }
-            
+        }
+        
+        //find metadata that has been mailed(cronned) more than 2 minggu but still x diusik
+        $result2 = MetadataGeo::on('pgsql2')->whereRaw('createdate = changedate')->whereDate('cronned_metadata_tak_diusik','<',$lastTwoWeeks)->get();
+        if(count($result2) > 0){
+            foreach($result2 as $r){
+                $r->cronned_metadata_tak_diusik = date('Y-m-d H:i:s',time());
+                $r->update();
+            }
+        }
+        
+        if(count($result1) > 0 || count($result2) > 0){
+            //send email notification to them pengesahs
             $pengesahs = User::whereHas("roles", function ($q) {
-                        $q->where("name", "Pengesah Metadata");
-                    })->get();
-                    
+                            $q->where("name", "Pengesah Metadata");
+                        })->get();
             if(count($pengesahs) > 0){
                 foreach($pengesahs as $p){
                     $to_name = $p->name;
@@ -68,6 +81,7 @@ class UncheckedMetadataCron extends Command
                     });            
                 }
             }
+            \Log::info("UncheckedMetadataCron executed!");
         }
     }
 }
