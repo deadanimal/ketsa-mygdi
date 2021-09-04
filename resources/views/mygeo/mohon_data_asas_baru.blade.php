b87632fdc3cc96ae15d6de0ee8ab517407e85483
farhanrimfiel <farhan.rimfiel@pipeline-network.com>
2021-09-04 12:05:16 +0800

    add visitor counter

--- resources/views/mygeo/mohon_data_asas_baru.blade.php
+++ resources/views/mygeo/mohon_data_asas_baru.blade.php
@@ -180,13 +180,7 @@
                                             </div>
                                             <div class="col-3">
                                                 <input class="form-control form-control-sm ml-3" type="text"
-                                                @if($pemohon->users->kategori == '2_g2e_iptsPelajar')
-                                                    value="G2E - (IPTS) Pelajar"
-                                                @elseif($pemohon->users->kategori == '2_g2e_iptaPelajar')
-                                                    value="G2E - (IPTA) Pelajar"
-                                                @else
-                                                value="-"
-                                                @endif disabled />
+                                                    value="{{ $pemohon->users->kategori }}" disabled />
                                             </div>
                                         </div>
                                     </div>
